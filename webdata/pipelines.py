0485453163346d3a8cfc3ad9fd14c16f79d486f4
hyc <rogwel@vip.qq.com>
2020-04-19 15:15:16 +0800

    origin

--- webdata/pipelines.py
+++ webdata/pipelines.py
@@ -12,22 +12,32 @@ class WebdataPipeline(object):
         self.session = loadSession()
 
     def process_item(self, item, spider):
-        a = anime.Anime(
-            id=item['_id'],
-            title=item['title'],
-            link=item['link'],
-            process=item['process'],
-            cover=item['cover'][0],
-            play_count=item['play_count'],
-            source=item['source']
-        )
         try:
-            self.session.add(a)
-            self.session.commit()
-            print("保存成功")
-        except:
-            print("id已存在:[{}]".format(item['_id']))
+            newA = self.session.query(anime.Anime).filter(anime.Anime.id == item['_id']).first()
+            if newA:
+                print("id已存在:[{}]".format(item['_id']))
+                newA.process = item['process']
+                newA.play_count = item['play_count']
+                self.session.commit()
+                print("更新成功")
+            else:
+                a = anime.Anime(
+                    id=item['_id'],
+                    title=item['title'],
+                    link=item['link'],
+                    process=item['process'],
+                    cover=item['cover'][0],
+                    play_count=item['play_count'],
+                    source=item['source']
+                )
+                self.session.add(a)
+                self.session.commit()
+                print("保存成功")
+        except Exception as e:
+            print(e)
             self.session.rollback()
+
+
         # print(item)
         return item
 
